cdf9e60670517f71ad355adeee37ec2650d14918
sdocy503 <docystephen@gmail.com>
2018-10-30 20:42:57 -0700

    Basic bot is done

---
+++ drake-bot/RiskBot/Modulues/ping.cs
@@ -0,0 +1,53 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Addons.Interactive;
+using System.Threading;
+
+namespace RiskBot.Modulues
+{
+    public class drakeBot : InteractiveBase
+    {
+        [Command("begonethots", RunMode = RunMode.Async)]
+        public async Task begone()
+        {
+            bool canKick = false;
+            foreach(var role in Context.Guild.GetUser(Context.User.Id).Roles)
+            {
+                if(role.Name == "House Tennant")
+                {
+                    canKick = true;
+                }
+            }
+            if (canKick)
+            {
+                var users = Context.Guild.Users;
+                bool isHouseMember = false;
+
+                foreach (var user in users)
+                {
+                    foreach (var role in user.Roles)
+                    {
+                        if (role.Name == "House Tennant")
+                        {
+                            isHouseMember = true;
+                        }
+                    }
+                    if (!isHouseMember && !user.IsBot)
+                    {
+                        try
+                        {
+                            await user.KickAsync("Not house member");
+                        }
+                        catch
+                        {
+                            await Context.Channel.SendMessageAsync($"Failed to kick {user.Mention}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
